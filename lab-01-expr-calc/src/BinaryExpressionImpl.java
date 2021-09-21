976b5715a3ffb512aaad59f3ac359a0f0c95020c
mrboorger <lolov.91@bk.ru>
2021-09-21 10:07:52 +0300

    Removed setters from BinaryExpressions and its variables were made final.

--- lab-01-expr-calc/src/BinaryExpressionImpl.java
+++ lab-01-expr-calc/src/BinaryExpressionImpl.java
@@ -1,20 +1,12 @@
 public class BinaryExpressionImpl implements BinaryExpression {
-    private OpType mOperationType;
-    private Expression mLeftChild;
-    private Expression mRightChild;
+    private final OpType mOperationType;
+    private final Expression mLeftChild;
+    private final Expression mRightChild;
 
     public BinaryExpressionImpl(OpType operationType, Expression left, Expression right) {
         mOperationType = operationType;
         mLeftChild = left;
         mRightChild = right;
-    }
-
-    public void setmLeftChild(Expression leftChild) {
-        this.mLeftChild = leftChild;
-    }
-
-    public void setmRightChild(Expression rightChild) {
-        this.mRightChild = rightChild;
     }
 
     @Override
