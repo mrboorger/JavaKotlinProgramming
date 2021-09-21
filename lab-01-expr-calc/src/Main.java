ff13792a63add2760adc986c5c979d7b105b63ac
mrboorger <lolov.91@bk.ru>
2021-09-21 21:47:26 +0300

    Added if-clause for run tests

--- lab-01-expr-calc/src/Main.java
+++ lab-01-expr-calc/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // variable must start with letter
+        if (args.length > 0 && args[0].equals("enabletests")) {
+            new TesterImpl().runAllTests();
+        }
         new RequestHandlerImpl().request();
     }
 }
