f1793b1c2a99de10f14c7cb834f435c497de9fee
mrboorger <lolov.91@bk.ru>
2021-09-21 10:25:25 +0300

    Added pareserDebugRepresentationTesterClass and some tests.

--- lab-01-expr-calc/src/TesterImpl.java
+++ lab-01-expr-calc/src/TesterImpl.java
@@ -27,26 +27,49 @@ public class TesterImpl implements Tester {
 
         TesterImpl.parserExceptionTester.test(parser, "+ 2 2");
         TesterImpl.parserExceptionTester.test(parser, "- 1 2");
-        TesterImpl.parserExceptionTester.test(parser, "1 + 2 - -");
+        TesterImpl.parserExceptionTester.test(parser, "1 + 2 - - 2 2");
 
         TesterImpl.parserExceptionTester.test(parser, "\n 1 - 2  - -");
         TesterImpl.parserExceptionTester.test(parser, "\n 1 - - 3 3");
         TesterImpl.parserExceptionTester.test(parser, "2 + 2 + xssd_dwD -   fg   -");
     }
 
+    private static class parserDebugRepresentationTester {
+        private static void test(ParserImpl parser, String strExpr, String expected) {
+            Expression root = null;
+            try {
+                root = parser.parseExpression(strExpr);
+            } catch (ExpressionParseException e) {
+                assert true : "Unexpected ExpressionParseException on " + strExpr + e.getMessage();
+            }
+            assert root != null : "Unexpected null root";
 
+            String result = (String)root.accept(new DebugRepresentationExpressionVisitor());
+            assert result.equals(expected) : result + "!=" + expected;
+        }
+    }
 
     private static void testDebugRepresentation() {
         var parser = new ParserImpl();
-        Expression root = null;
-        var strExpr = "-1 - 2 + 3.123 + Xsd + sd0dd - 0.002333033";
-        try {
-            root = parser.parseExpression(strExpr);
-        } catch (ExpressionParseException e) {
-            assert true : "Unexpected ExpressionParseException on " + strExpr + e.getMessage();
+        {
+            String strExpr = "92.3";
+            String expected = "CON[92.3]";
+            parserDebugRepresentationTester.test(parser,strExpr, expected);
         }
-        assert root != null : "Unexpected null root";
-
-        System.out.println(root.accept(new DebugRepresentationExpressionVisitor()));
+        {
+            String strExpr = "X_d022";
+            String expected = "VAR[X_d022]";
+            parserDebugRepresentationTester.test(parser,strExpr, expected);
+        }
+        {
+            String strExpr = "X_d022 - 92.3";
+            String expected = "SUB(VAR[X_d022], CON[92.3])";
+            parserDebugRepresentationTester.test(parser,strExpr, expected);
+        }
+        {
+            String strExpr = "x_w + 2.3 - 0.22";
+            String expected = "SUB(ADD(VAR[x_w], CON[2.3]), CON[0.22])";
+            parserDebugRepresentationTester.test(parser,strExpr, expected);
+        }
     }
 }
