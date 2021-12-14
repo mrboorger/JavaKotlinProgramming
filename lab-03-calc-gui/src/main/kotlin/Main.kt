32fbf3f6da08604726da5690135e2fe8fd716658
mrboorger <lolov.91@bk.ru>
2021-12-14 19:45:38 +0300

    Fixed CodeStyle

--- lab-03-calc-gui/src/main/kotlin/Main.kt
+++ lab-03-calc-gui/src/main/kotlin/Main.kt
@@ -1,18 +1,4 @@
-import java.awt.GridLayout
-import javax.swing.JButton
-import javax.swing.JFrame
-import javax.swing.JPanel
-import javax.swing.JTextField
-
-
 fun main(args: Array<String>) {
     val window = MainWindow()
-
-//    window.getContentPane().add(keyboard)
     window.setVisible(true)
-
-
-//    val parser = ParserImpl()
-//    var expr = parser.parseExpression("1 + 3 - 2")
-//    println(expr.accept(ComputeExpressionVisitor.INSTANCE))
 }
